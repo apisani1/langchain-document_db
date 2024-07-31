6cc93496a8dd1adb5dec0a78e72c8b5e182c8f81
Antonio Pisani <antonio.pisani@gmail.com>
2024-07-31 09:40:42 -0400

    Add support for different document splitters

--- document_loaders/load_document.py
+++ document_loaders/load_document.py
@@ -1,159 +1,138 @@
 import os
+from pathlib import Path
 from typing import (
     Any,
-    Optional,
+    Union,
 )
 
 from langchain.docstore.document import Document
 from langchain_community.document_loaders.base import BaseLoader
+from langchain_community.document_loaders.text import TextLoader
 
 from .text_splitter import chunkable
 
 
 @chunkable
-def load_document(file_path: str, *args: Any, **kwargs: Any) -> list[Document]:
+def load_document(
+    file_path: Union[str, Path], *args: Any, **kwargs: Any
+) -> list[Document]:
     """
-    Load individual files an covert it into a list of Lanchain documents.
+    Load an individual file and covert it into a list of Lanchain documents.
 
     The corresponding document loader is selected acording to the file extension. If a specific document loader
-    is not available tries with the Unstructured file loader.
+    is not available tries with the UnstructuredFileLoader.
 
     Args:
-        file_path (str): File path or url address of the document to load.
-        metadata (Optional[dict], optional): Metadata to add to chunks. Defaults to None.
-        chunk_it (bool, optional): Whether to chuck the documents. Defaults to False.
-        text_splitter (TextSplitter, optional): Text splitter to use. Defaults to RecursiveCharacterTextSplitter.
-        chunk_size (int, optional): Maximum size of chunks to return, Defaults to 4000.
-        chunk_overlap (int, optional): Overlap in characters between chunks. Defaults to 200.
-        separators (list, optional): List of strings with separators. Defaults to None.
-                                        If None uses ["\n\n", "\n", " "]
-        length_function (func, optional): Function that measures the length of given chunks. Defaults to len.
-        keep_separator (bool, optional): Whether to keep the separator in the chunks. Defaults to True.
-        is_separator_regex (bool, optional): Wheter the separator is a regular expression. Defaults to False.
-        add_start_index (bool, optional): If True, includes chunk's start index in metadata. Defaults to False.
-
+        file_path (str | Path): File path of the document to load.
+        text_splitter (Union[str, DocumentSplitterType], optional): Text splitter to use to chunks the document into
+            smaller documents. Possible values are:
+                "auto": Automatically determine the splitter based on the file extension.
+                "character" or CharacterTextSplitter,
+                "markdown" or MarkdownHeaderTextSplitter,
+                "recursive" or RecursiveCharacterTextSplitter,
+                "semantic" or SemanticChunker,
+                "c", "cpp", "csharp", "cobol",  "elixir", "go", "haskell", "html", "java", "js", "json", "kotlin",
+                "latex", "lua", "php", "perl", "proto", "python", "rst", "ruby", "rust", "scala", "sol", "swift" "ts"
+        splitter_kwargs (dict, optional): Keyword arguments to pass to the text splitter. Defaults to None.
     Returns:
         List of Langchain documents.
     """
     loader: BaseLoader
 
-    _, extension = os.path.splitext(file_path)
-    match extension:
-        case ".txt":
-            from langchain_community.document_loaders.text import TextLoader
+    if "mode" in kwargs and kwargs["mode"] == "raw":
+        kwargs.pop("mode")
+        if "encoding" not in kwargs and "autodetect_encoding" not in kwargs:
+            kwargs["autodetect_encoding"] = True
 
-            loader = TextLoader(file_path, *args, **kwargs)
-        case ".pdf":
-            from langchain_community.document_loaders.pdf import PyPDFLoader
+        loader = TextLoader(file_path, *args, **kwargs)
+    else:
+        _, extension = os.path.splitext(file_path)
+        match extension:
+            case ".txt":
+                if "encoding" not in kwargs and "autodetect_encoding" not in kwargs:
+                    kwargs["autodetect_encoding"] = True
 
-            loader = PyPDFLoader(file_path, *args, **kwargs)
-        case ".docx" | ".doc":
-            from langchain.document_loaders.word_document import Docx2txtLoader
+                loader = TextLoader(file_path, *args, **kwargs)
+            case ".pdf":
+                from langchain.document_loaders.unstructured import (
+                    UnstructuredFileLoader,
+                )
 
-            loader = Docx2txtLoader(file_path, *args, **kwargs)
-        case ".csv":
-            from langchain.document_loaders.csv_loader import CSVLoader
+                if "mode" not in kwargs:
+                    kwargs["mode"] = "single"
+                if "strategy" not in kwargs:
+                    kwargs["strategy"] = "fast"
+                if "load_tables" not in kwargs:
+                    kwargs["load_tables"] = False
+                elif kwargs["load_tables"]:
+                    kwargs["skip_infer_table_types"] = []
+                    kwargs["pdf_infer_table_structure"] = True
 
-            loader = loader = CSVLoader(file_path, *args, **kwargs)
-        case ".eml":
-            from langchain.document_loaders.email import UnstructuredEmailLoader
+                loader = UnstructuredFileLoader(file_path, *args, **kwargs)
+            case ".docx" | ".doc":
+                from langchain.document_loaders.word_document import Docx2txtLoader
 
-            loader = UnstructuredEmailLoader(file_path, *args, **kwargs)
-        case ".epub":
-            from langchain.document_loaders.epub import UnstructuredEPubLoader
+                loader = Docx2txtLoader(file_path, *args, **kwargs)
+            case ".csv":
+                from langchain.document_loaders.csv_loader import CSVLoader
 
-            loader = UnstructuredEPubLoader(file_path, *args, **kwargs)
-        case ".xlsx" | ".xls":
-            # data = load_document(file_path_name, mode='elements')
-            from langchain.document_loaders.excel import UnstructuredExcelLoader
+                loader = loader = CSVLoader(file_path, *args, **kwargs)
+            case ".eml":
+                from langchain.document_loaders.email import UnstructuredEmailLoader
 
-            loader = UnstructuredExcelLoader(file_path, *args, **kwargs)
-        case ".pptx" | ".ppt":
-            # data = load_document(file_path_name, mode='elements')
-            from langchain.document_loaders.powerpoint import (
-                UnstructuredPowerPointLoader,
-            )
+                loader = UnstructuredEmailLoader(file_path, *args, **kwargs)
+            case ".epub":
+                from langchain.document_loaders.epub import UnstructuredEPubLoader
 
-            loader = UnstructuredPowerPointLoader(file_path, *args, **kwargs)
-        case ".srt":
-            from langchain.document_loaders.srt import SRTLoader
+                loader = UnstructuredEPubLoader(file_path, *args, **kwargs)
+            case ".xlsx" | ".xls":
+                # data = load_document(file_path_name, mode='elements')
+                from langchain.document_loaders.excel import UnstructuredExcelLoader
 
-            loader = SRTLoader(file_path, *args, **kwargs)
-        case ".html":
-            from langchain.document_loaders.html import UnstructuredHTMLLoader
+                loader = UnstructuredExcelLoader(file_path, *args, **kwargs)
+            case ".pptx" | ".ppt":
+                # data = load_document(file_path_name, mode='elements')
+                from langchain.document_loaders.powerpoint import (
+                    UnstructuredPowerPointLoader,
+                )
 
-            loader = UnstructuredHTMLLoader(file_path, *args, **kwargs)
-        case ".json":
-            # data = load_document(file_path_name, jq_schema='.', text_content=False)
-            from langchain.document_loaders.json_loader import JSONLoader
+                loader = UnstructuredPowerPointLoader(file_path, *args, **kwargs)
+            case ".srt":
+                from langchain.document_loaders.srt import SRTLoader
 
-            loader = JSONLoader(file_path, *args, **kwargs)
-        case ".md":
-            # data = load_document(file_path_name, mode='elements')
-            from langchain.document_loaders.markdown import UnstructuredMarkdownLoader
+                loader = SRTLoader(file_path, *args, **kwargs)
+            case ".html":
+                from langchain.document_loaders.html import UnstructuredHTMLLoader
 
-            loader = UnstructuredMarkdownLoader(file_path, *args, **kwargs)
-        case ".ipynb":
-            from langchain.document_loaders.notebook import NotebookLoader
+                if "mode" not in kwargs:
+                    kwargs["mode"] = "single"
+                if "strategy" not in kwargs:
+                    kwargs["strategy"] = "fast"
 
-            loader = NotebookLoader(file_path, *args, **kwargs)
-        case _:
-            from langchain.document_loaders.unstructured import UnstructuredFileLoader
+                loader = UnstructuredHTMLLoader(file_path, *args, **kwargs)
+            case ".json":
+                from langchain.document_loaders.json_loader import JSONLoader
 
-            loader = UnstructuredFileLoader(file_path, *args, **kwargs)
-    return loader.load()
+                if "jq_schema" not in kwargs:
+                    kwargs["jq_schema"] = "."
+                if "text_content" not in kwargs:
+                    kwargs["text_content"] = False
 
+                loader = JSONLoader(file_path, *args, **kwargs)
+            case ".md":
+                from langchain.document_loaders.markdown import (
+                    UnstructuredMarkdownLoader,
+                )
 
-@chunkable
-def load_unstructured_document(
-    file_path: str,
-    mode: str = "single",
-    load_tables: bool = False,
-    chunking_strategy: Optional[str] = None,
-    **kwargs: Any,
-) -> list[Document]:
-    """
-    The file loader uses the unstructured partition function and will automatically detect the file
-    type.
+                loader = UnstructuredMarkdownLoader(file_path, *args, **kwargs)
+            case ".ipynb":
+                from langchain.document_loaders.notebook import NotebookLoader
 
-    Args:
-        mode: You can run the loader in one of three modes: "single", "paged", and "elements".
-              If you use "single" mode, the document will be returned as a single langchain Document object.
-              If you use "paged" mode, the document will be splitted by page.
-              If you use "elements" mode, the unstructured library will split the document into elements
-              such as Title and NarrativeText.
-        load_tables: Set it to True for unstructured infering the structure of tables automatically.
-        metadata (Optional[dict], optional): Metadata to add to chunks. Defaults to None.
-        chunk_it (bool, optional): Whether to chuck the documents. Defaults to False.
-        text_splitter (TextSplitter, optional): Text splitter to use. Defaults to RecursiveCharacterTextSplitter.
-        chunk_size (int, optional): Maximum size of chunks to return, Defaults to 4000.
-        chunk_overlap (int, optional): Overlap in characters between chunks. Defaults to 200.
-        separators (list, optional): List of strings with separators. Defaults to None.
-                                        If None uses ["\n\n", "\n", " "]
-        length_function (func, optional): Function that measures the length of given chunks. Defaults to len.
-        keep_separator (bool, optional): Whether to keep the separator in the chunks. Defaults to True.
-        is_separator_regex (bool, optional): Wheter the separator is a regular expression. Defaults to False.
-        add_start_index (bool, optional): If True, includes chunk's start index in metadata. Defaults to False.
+                loader = NotebookLoader(file_path, *args, **kwargs)
+            case _:
+                from langchain.document_loaders.unstructured import (
+                    UnstructuredFileLoader,
+                )
 
-        You can pass in additional unstructured kwargs after mode to apply different unstructured settings. The
-        following arguments manage the chunking strategy of unstructed. It may be redundant to use both.
-
-        chunking_strategy: Strategy used for chunking text into larger or smaller elements. Defaults to `None` with
-                           optional arg of 'by_title'
-        multipage_sections: If True, sections can span multiple pages. Defaults to True.
-        combine_text_under_n_chars: Combines elements (for example a series of titles) until a section reaches
-                                    a length of n characters.
-        new_after_n_chars: Cuts off new sections once they reach a length of n characters, a soft max.
-        max_characters: Chunks elements text and text_as_html (if present) into chunks of length n characters,
-                        a hard max.
-    Returns:
-        List of Langchain documents.
-    """
-    from langchain.document_loaders.unstructured import UnstructuredFileLoader
-
-    if load_tables:
-        kwargs.update({"skip_infer_table_types": [], "pdf_infer_table_structure": True})
-    loader = UnstructuredFileLoader(
-        file_path, mode=mode, chunking_strategy=chunking_strategy, **kwargs
-    )
+                loader = UnstructuredFileLoader(file_path, *args, **kwargs)
     return loader.load()
--- document_loaders/text_splitter.py
+++ document_loaders/text_splitter.py
@@ -1,3 +1,5 @@
+import json
+import os
 from functools import wraps
 from typing import (
     Any,
@@ -12,54 +14,149 @@ from typing import (
 from langchain.docstore.document import Document
 from langchain.text_splitter import (
     CharacterTextSplitter,
-    HTMLHeaderTextSplitter,
-    MarkdownHeaderTextSplitter,
     Language,
+    MarkdownTextSplitter,
     RecursiveCharacterTextSplitter,
     RecursiveJsonSplitter,
     TextSplitter,
 )
 from langchain_experimental.text_splitter import SemanticChunker
+from langchain_text_splitters.html import HTMLSectionSplitter
+
+
+class HTMLSplitter(HTMLSectionSplitter):
+    def __init__(self, *args, **kwargs):
+        if "headers_to_split_on" not in kwargs:
+            kwargs["headers_to_split_on"] = [
+                ("h1", "Header 1"),
+                ("h2", "Header 2"),
+                ("h3", "Header 3"),
+                ("h4", "Header 4"),
+                ("h5", "Header 5"),
+                ("h6", "Header 6"),
+            ]
+        # if "return_each_element" not in kwargs:
+        #     kwargs["return_each_element"] = True
+        super().__init__(*args, **kwargs)
+
+    def split_documents(self, documents: Iterable[Document]) -> List[Document]:
+        html_docs = []
+        for doc in documents:
+            try:
+                sub_docs = self.split_text(doc.page_content)
+                for sub_doc in sub_docs:
+                    sub_doc.metadata.update(doc.metadata)
+                html_docs.extend(sub_docs)
+            except Exception:
+                html_docs.append(doc)
+        return html_docs
+
+
+class JsonSplitter(RecursiveJsonSplitter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def split_documents(self, documents: Iterable[Document]) -> List[Document]:
+        json_docs = []
+        for doc in documents:
+            try:
+                json_data = json.loads(doc.page_content)
+                sub_docs = [
+                    Document(page_content=json.dumps(chunk), metadata=doc.metadata)
+                    for chunk in self.split_json(json_data)
+                ]
+                json_docs.extend(sub_docs)
+            except json.JSONDecodeError:
+                json_docs.append(doc)
+        return json_docs
+
 
 DocumentSplitterType = Union[
     CharacterTextSplitter,
-    HTMLHeaderTextSplitter,
-    MarkdownHeaderTextSplitter,
+    HTMLSplitter,
+    JsonSplitter,
+    MarkdownTextSplitter,
     RecursiveCharacterTextSplitter,
-    RecursiveJsonSplitter,
-    TextSplitter,
-    SemanticChunker
+    SemanticChunker,
 ]
 
 DocumentSplitter = (
     CharacterTextSplitter,
-    HTMLHeaderTextSplitter,
-    MarkdownHeaderTextSplitter,
+    HTMLSplitter,
+    JsonSplitter,
+    MarkdownTextSplitter,
     RecursiveCharacterTextSplitter,
-    RecursiveJsonSplitter,
-    SemanticChunker
+    SemanticChunker,
 )
 
-def _get_splitter(text_splitter: Union[str, DocumentSplitterType], **kwargs) -> DocumentSplitterType:
+extension_to_type = {
+    '.html': "html",
+    '.json': "json",
+    '.md': "markdown",
+    '.go': "go",
+    '.java': "java",
+    '.kt': "kotlin",
+    '.js': "js",
+    '.ts': "ts",
+    '.php': "php",
+    '.proto': "proto",
+    '.py': "python",
+    '.rst': "rst",
+    '.rb': "ruby",
+    '.rs': "rust",
+    '.scala': "scala",
+    '.swift': "swift",\
+    '.tex': "latex",
+    '.sol': "sol",
+    '.cs': "csharp",
+    '.cob': "cobol",
+    '.c': "c",
+    '.lua': "lua",
+    '.pl': "perl",
+    '.hs': "haskell",
+    '.ex': "elixir",
+    '.exs': "elixir"
+}
+
+
+def _find_splitter_type(documents: Iterable[Document]) -> str:
+    if len(documents) != 0 and "source" in documents[0].metadata:
+        _, extension = os.path.splitext(documents[0].metadata["source"])
+        if extension in extension_to_type:
+            return extension_to_type[extension]
+    return "recursive"
+
+
+def _get_splitter(
+    text_splitter: Union[str, DocumentSplitterType],
+    documents: Iterable[Document],
+    **kwargs,
+) -> DocumentSplitterType:
     if isinstance(text_splitter, DocumentSplitter):
         return text_splitter
     if isinstance(text_splitter, str):
+        if text_splitter == "auto":
+            text_splitter = _find_splitter_type(documents)
         match text_splitter:
             case "character":
                 return CharacterTextSplitter(**kwargs)
             case "html":
-                return HTMLHeaderTextSplitter(**kwargs)
+                return HTMLSplitter(**kwargs)
+            case "json":
+                return JsonSplitter(**kwargs)
             case "markdown":
-                return MarkdownHeaderTextSplitter(**kwargs)
+                return MarkdownTextSplitter(**kwargs)
             case "recursive":
                 return RecursiveCharacterTextSplitter(**kwargs)
-            case "json":
-                return RecursiveJsonSplitter(**kwargs)
             case "semantic":
                 return SemanticChunker(**kwargs)
             case _:
-                pass
-    raise ValueError("Bad text_splitter for chunk_docs")
+                if text_splitter in Language:
+                    return RecursiveCharacterTextSplitter.from_language(
+                        language=Language(text_splitter), **kwargs
+                    )
+    raise ValueError(f"Bad text_splitter for chunk_docs: {text_splitter}")
+
 
 def chunk_docs(
     documents: Iterable[Document],
@@ -74,26 +171,27 @@ def chunk_docs(
 
     Args:
         documents (Iterable[Document]): List of Langchain documents to chunk.
-        text_splitter (Union[str, DocumentSplitterType], optional): Text splitter to use.
-            Possible values are:
-                "character" or CharacterTextSplitter
-                "html" or HTMLHeaderTextSplitter
-                "markdown" or MarkdownHeaderTextSplitter
-                "recursive" or RecursiveCharacterTextSplitter
-                "json" or RecursiveJsonSplitter
-                "semantic" or SemanticChunker
-        metadata (Optional[Dict], optional): Metadata to add to chunks. Defaults to None.
+        text_splitter (Union[str, DocumentSplitterType], optional): Text splitter to use to chunks the document into
+            smaller documents. Possible values are:
+                "auto": Automatically determine the splitter based on the file extension.
+                "character" or CharacterTextSplitter,
+                "markdown" or MarkdownHeaderTextSplitter,
+                "recursive" or RecursiveCharacterTextSplitter,
+                "semantic" or SemanticChunker,
+                "c", "cpp", "csharp", "cobol",  "elixir", "go", "haskell", "html", "java", "js", "json", "kotlin",
+                "latex", "lua", "php", "perl", "proto", "python", "rst", "ruby", "rust", "scala", "sol", "swift" "ts".
         **kwargs: Additional keyword arguments to pass to the text splitter.
 
     Returns:
         List of Langchain documents.
     """
-    text_splitter = _get_splitter(text_splitter, **kwargs)
+    text_splitter = _get_splitter(text_splitter, documents, **kwargs)
     chunks = text_splitter.split_documents(documents)
     if metadata:
         for chunk in chunks:
             chunk.metadata.update(metadata)
     return chunks
+
 
 def chunkable(func: Callable) -> Callable:
     @wraps(func)
@@ -108,17 +206,18 @@ def chunkable(func: Callable) -> Callable:
         Decorator that adds chunking capability to a function that returns a list of lamngchain Documents.
 
         Args:
-            text_splitter (Union[str, DocumentSplitterType], optional): Text splitter to use.
-                Possible values are:
-                    "character" or CharacterTextSplitter
-                    "html" or HTMLHeaderTextSplitter
-                    "markdown" or MarkdownHeaderTextSplitter
-                    "recursive" or RecursiveCharacterTextSplitter
-                    "json" or RecursiveJsonSplitter
-                    "semantic" or SemanticChunker
-            metadata (dict, optional): Metadata to add to each chunk. Defaults to None.
+            text_splitter (Union[str, DocumentSplitterType], optional): Text splitter to use to chunks the document into
+                smaller documents. Possible values are:
+                    "auto": Automatically determine the splitter based on the file extension.
+                    "character" or CharacterTextSplitter,
+                    "markdown" or MarkdownHeaderTextSplitter,
+                    "recursive" or RecursiveCharacterTextSplitter,
+                    "semantic" or SemanticChunker,
+                    "c", "cpp", "csharp", "cobol",  "elixir", "go", "haskell", "html", "java", "js", "json", "kotlin",
+                    "latex", "lua", "php", "perl", "proto", "python", "rst", "ruby", "rust", "scala", "sol", "swift" "ts"
             splitter_kwargs (dict, optional): Keyword arguments to pass to the text splitter. Defaults to None.
         """
+        splitter_kwargs = splitter_kwargs or {}
         result = func(*args, **kwargs)
         if result and text_splitter:
             return chunk_docs(
